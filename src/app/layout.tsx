e893645fc0adfd19653ed7e104baa0d1a21975e5
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2023-02-23 17:57:43 -0300

    commit para continuar em outro computador

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
-import './globals.css'
+import { ReactNode } from "react";
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children, }: { children: ReactNode}) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
