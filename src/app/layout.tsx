c42fac64925545f331cd420fa55e915c3952b631
Ronald Muller <ronald.muller@silsistemas.com.br>
2023-02-24 12:48:13 -0300

    continuacao

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { Head } from "next/document";
 import { ReactNode } from "react";
 
 export default function RootLayout({ children, }: { children: ReactNode}) {
@@ -7,7 +8,11 @@ export default function RootLayout({ children, }: { children: ReactNode}) {
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
