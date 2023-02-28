0ce5cd735a2f39fe740d44d3e7e7000f409b5f99
Ronald Deived Muller <rdm.ronaldmuller@gmail.com>
2023-02-28 07:42:46 -0300

    erro static props

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import { Head } from "next/document";
+import { globalStyles } from "@/styles/global";
 import { ReactNode } from "react";
+import { getCssText } from "../styles";
+
+globalStyles();
 
 export default function RootLayout({ children, }: { children: ReactNode}) {
-  return (
-    <html lang="pt-BR">
-      {/*
+	return (
+		<html lang="pt-BR">
+			{/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+			<head>
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
-      </head>
-      <body>{children}</body>
-    </html>
-  )
+				<style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+			</head>
+			<body>{children}</body>
+		</html>
+	);
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,79 @@
-export default function Home() {
-  return (
-    <h1>Hello world</h1>
-  )
+import { stripe } from "@/lib/stripe";
+import { GetStaticProps } from "next";
+import Stripe from "stripe";
+
+interface HomeProps {
+	products: {
+		id: string;
+		name: string;
+		imageUrl: string;
+		price: string;
+	}[]
+} 
+ 
+export default async function Home(/*{ products }: HomeProps*/) {
+	/*const response = await stripe.products.list({
+		expand: ["data.default_price"],
+	});
+
+	const products: HomeProps = response.data.map(product => {
+		const price = product.default_price as Stripe.Price;
+
+		return {
+			id: product.id,
+			name: product.name,
+			imageUrl: product.images[0],      
+			price: new Intl.NumberFormat("pt-BR", {
+				style: "currency",
+				currency: "BRL",
+			}).format(price.unit_amount / 100),
+		};
+	});*/
+
+	//console.log(products);
+
+	return (
+		<>
+			<h1>Hello world</h1>
+			<ul>
+				{/*products.map((product) => {
+					return (
+						<li key={product.id}>{product.name}</li>
+					);
+				})*/}
+			</ul>
+		</>
+	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+//export async function getStaticProps(context) {
+	console.log(">>>>>>> teste <<<<<<<<<<<<<");
+
+	const response = await stripe.products.list({
+		expand: ["data.default_price"]
+	});
+
+	const products = response.data.map(product => {
+		const price = product.default_price as Stripe.Price;
+
+		return {
+			id: product.id,
+			name: product.name,
+			imageUrl: product.images[0],      
+			price: new Intl.NumberFormat("pt-BR", {
+				style: "currency",
+				currency: "BRL",
+			}).format(price.unit_amount / 100),
+		};
+	});
+
+	console.log(products);
+
+	return {
+		props: {
+			products: products,
+		},
+		revalidate: 60 * 60 * 2, // a cada 2 horas gera uma nova pagina no server SSG
+	};
+};
